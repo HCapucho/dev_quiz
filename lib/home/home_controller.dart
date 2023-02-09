b5f529c947ae5175ddbab18a319a5fd383b18e02
Hugo Capucho <capuchop@live.com>
2023-02-09 12:51:06 -0300

    Completo

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,30 @@
+import 'package:dev_quiz/home/home_repository.dart';
+import 'package:dev_quiz/home/home_state.dart';
+import 'package:dev_quiz/shared/models/quiz_model.dart';
+import 'package:dev_quiz/shared/models/user_model.dart';
+import 'package:flutter/material.dart';
+
+class HomeController {
+  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
+
+  // USER
+  // QUIZZES
+  UserModel? user;
+  List<QuizModel>? quizzes;
+
+  final repository = HomeRepository();
+
+  void getUser() async {
+    state = HomeState.loading;
+    user = await repository.getUser();
+    state = HomeState.success;
+  }
+
+  void getQuizzes() async {
+    state = HomeState.loading;
+    quizzes = await repository.getQuizzes();
+    state = HomeState.success;
+  }
+}
---
+++ lib/home/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:dev_quiz/challenge/challenge_page.dart';
+import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/home_controller.dart';
+import 'package:dev_quiz/home/home_state.dart';
+import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(user: controller.user!),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 19),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(label: "Fácil"),
+                  LevelButtonWidget(label: "Médio"),
+                  LevelButtonWidget(label: "Difícil"),
+                  LevelButtonWidget(label: "Perito"),
+                ],
+              ),
+              SizedBox(height: 24),
+              Expanded(
+                  child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: controller.quizzes!
+                    .map((e) => QuizCardWidget(
+                          title: e.title,
+                          percent: e.questionAnswered / e.questions.length,
+                          completed:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => ChallengePage(
+                                questions: e.questions,
+                                title: e.title,
+                              ),
+                            ));
+                          },
+                        ))
+                    .toList(),
+              ))
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
+  }
+}
